9d773d31db9a3d39150edaab3faaff72836f08bf
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-11-05 20:41:55 -0300

    first commit

---
+++ exercicio_modulo25.js
@@ -0,0 +1,13 @@
+const alunos = [
+    { nome: "João", nota: 5 },
+    { nome: "Maria", nota: 7 },
+    { nome: "Pedro", nota: 8 },
+    { nome: "Ana", nota: 4 },
+    { nome: "Carlos", nota: 6 },
+]
+
+const alunosAprovados = (alunos) => {
+    return alunos.filter (aluno => aluno.nota >= 6)
+}
+
+console.log(alunosAprovados(alunos))
